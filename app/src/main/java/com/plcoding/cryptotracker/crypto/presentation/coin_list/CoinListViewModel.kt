be8565fe35c675bc28fe841eb9f69ee781cd636d
Abdelrahman Elgendy <abdelrahmanelgendy961@gmail.com>
2025-02-09 23:50:25 +0200

    #8 Custom Line Chart + Adaptive Navigation

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,6 +6,7 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.crypto.data.networking.RemoteCoinDataSource
 import com.plcoding.cryptotracker.crypto.domain.CoinDataSource
+import com.plcoding.cryptotracker.crypto.presentation.coin_detail.DataPoint
 import com.plcoding.cryptotracker.crypto.presentation.models.CoinUi
 import com.plcoding.cryptotracker.crypto.presentation.models.toCoinUi
 import kotlinx.coroutines.channels.Channel
@@ -18,18 +19,15 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class CoinListViewModel(
     private val coinDataSource: CoinDataSource
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(CoinListState())
-    val state = _state
-        .onStart { loadCoins() }
-        .stateIn(
-            viewModelScope,
-            SharingStarted.WhileSubscribed(5000L),
-            CoinListState()
+    val state = _state.onStart { loadCoins() }.stateIn(
+            viewModelScope, SharingStarted.WhileSubscribed(5000L), CoinListState()
         )
 
     private val _events = Channel<CoinListEvent>()
@@ -38,12 +36,6 @@ class CoinListViewModel(
     fun onAction(action: CoinListAction) {
         when (action) {
             is CoinListAction.OnCoinClick -> {
-//                _state.update {
-//                    it.copy(
-//                        selectedCoin = action.coinUi
-//
-//                    )
-//                }
                 selectCoin(action.coinUi)
             }
         }
@@ -54,13 +46,26 @@ class CoinListViewModel(
 
         viewModelScope.launch {
             coinDataSource.getCoinHistory(
-                coinId = coinUi.id,
-                start = ZonedDateTime.now().minusDays(5),
-                end = ZonedDateTime.now()
-            ).onSuccess { history ->
-                print(history)
-            }
-                .onError { error ->
+                    coinId = coinUi.id,
+                    start = ZonedDateTime.now().minusDays(5),
+                    end = ZonedDateTime.now()
+                ).onSuccess { history ->
+                    val dataPoints = history.sortedBy { it.dateTime }.map {
+                            DataPoint(
+                                x = it.dateTime.hour.toFloat(),
+                                y = it.priceUsd.toFloat(),
+                                xLabel = DateTimeFormatter.ofPattern("ha\nM/d").format(it.dateTime)
+                            )
+                        }
+
+                    _state.update {
+                        it.copy(
+                            selectedCoin = it.selectedCoin?.copy(
+                                coinPriceHistory = dataPoints
+                            )
+                        )
+                    }
+                }.onError { error ->
                     _events.send(CoinListEvent.Error(error))
                 }
         }
@@ -74,17 +79,11 @@ class CoinListViewModel(
                 )
             }
 
-            coinDataSource
-                .getCoins()
-                .onSuccess { coins ->
+            coinDataSource.getCoins().onSuccess { coins ->
                     _state.update {
-                        it.copy(
-                            isLoading = false,
-                            coins = coins.map { it.toCoinUi() }
-                        )
+                        it.copy(isLoading = false, coins = coins.map { it.toCoinUi() })
                     }
-                }
-                .onError { error ->
+                }.onError { error ->
                     _state.update { it.copy(isLoading = false) }
                     _events.send(CoinListEvent.Error(error))
                 }
